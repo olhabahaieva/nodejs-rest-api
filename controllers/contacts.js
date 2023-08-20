be43e3e5c1b943198911fd2b812dc244d11ffb11
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-08-20 16:18:45 +0300

    removed schema from non-relevant contacts.js file

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -33,10 +33,6 @@ const removeContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
-  const { error } = addSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, "Missing fields");
-  }
   const { contactId } = req.params;
   const result = await contacts.updateContact(contactId, req.body);
   if (!result) {
