acfa3b5e1f4b3ebf609180bc2eee0d1938054b10
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-09-03 18:48:29 +0300

    added 409 email message error

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,12 @@ const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const register = async (req, res) => {
+    const {email} = req.body;
+    const user = await User.findOne({ email });
+
+    if(user){
+        throw HttpError(409, "Email in use")
+    }
     const newUser = await User.create(req.body);
 
     res.status(201).json({
