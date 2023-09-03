31e19e03efbda9dfe823210808bddb0fb4a64fb8
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-09-03 20:18:06 +0300

    added token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,11 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const { User } = require("../models/user");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
+
+const {SECRET_KEY} = process.env;
 
 const register = async (req, res) => {
     const {email, password} = req.body;
@@ -32,7 +36,11 @@ const login = async(req, res)=>{
     throw HttpError(401, "Email or password is wrong");
   }
 
-  const token = "fewfewfew.fewfewfew.fewfewui";
+  const payload = {
+    id: user._id,
+  }
+
+  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"})
 
   res.json({
     token,
