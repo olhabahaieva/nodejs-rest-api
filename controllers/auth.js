760a1a29c34dc0c1c2f166f16cdea7ecc3293872
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-09-13 16:49:03 +0300

    added Jimp for 250x250 image resizing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
+const Jimp = require("jimp");
 const path = require("path");
 const fs = require("fs/promises");
 
@@ -74,9 +75,13 @@ const logout = async(req, res, next) => {
 const updateAvatar = async(req, res)=> {
   const {_id} = req.user;
   const {path: tempUpload, originalname} = req.file;
+
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
-  await fs.rename(tempUpload, resultUpload);
+
+  await Jimp.read(tempUpload).then((img) => img.resize(250, 250).write(resultUpload));
+  await fs.unlink(tempUpload);
+  
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, {avatarURL});
 
